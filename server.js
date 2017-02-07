3a35178a1718a471654692187b53fe6f62751cc2
Arvin <akhadisurya@gmail.com>
2017-02-06 18:13:30 -0800

    edit index html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var PORT = 8000 || process.env.port;
+var PORT = 8000 || process.env.PORT;
 var mainRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
-var DB = "mongodb://localhost.angulartwo";
+var DB = "mongodb://localhost/angularHuman";
 var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -13,17 +13,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', mainRouter);
 app.use('/api', apiRouter);
-mongoose.connect(DB, function(err){
-  if(err){
-    return err;
-  } else {
-    console.log('Successfully connected to ' + DB);
-  }
+mongoose.connect(DB, function(err) {
+    if (err) {
+        return err;
+    } else {
+        console.log('Successfully connected to ' + DB);
+    }
 });
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', __dirname + '/client/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static(path.join(__dirname + '/client')));
-app.listen(PORT, function(){
-  console.log('Listening on port ' + PORT);
-})
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+app.listen(PORT, function() {
+    console.log('Listening on port ' + PORT);
+});
