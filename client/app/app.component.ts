c726ca6ca0bc83a0cc00389c386ad3a8a4b76205
Arvin <akhadisurya@gmail.com>
2017-02-08 14:58:30 -0800

    create app.component.js

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  template: '<h1>Hello from Angular 2</h1><human></human>'
+    selector: 'app',
+    template: '<h1> hello from angular2!!!</h1><human></human>'
 })
-
 export class AppComponent { }
